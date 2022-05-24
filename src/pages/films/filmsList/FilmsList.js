b0de8dcbadd2a33844a6d9d4a870378981914c4c
M-A <m-a@M-As-MacBook-Pro.local>
2022-05-24 06:56:18 +0200

    add search functionallity to FilmsList

--- src/pages/films/filmsList/FilmsList.js
+++ src/pages/films/filmsList/FilmsList.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import style from './filmsList.module.css';
 import FilmListItem from './FilmListItem';
+import SearchInput from '../../../components/SearchInput/SearchInput';
+import { useSearch } from '../../../hooks/useSearch';
+
 function FilmsList({ films, onSelect, selected }) {
+  const {
+    data: searchResults,
+    onChange,
+    searchText,
+  } = useSearch(films, 'title');
+
   return (
     <>
-      {films?.map((film) => (
+      <SearchInput value={searchText} onChange={onChange} />
+      {searchResults?.map((film) => (
         <FilmListItem
           key={film.episode_id}
           film={film}
