ebf0a089fbc2b3c9b2142a556310dfe050dad6d9
M-A <m-a@M-As-MacBook-Pro.local>
2022-05-24 06:44:28 +0200

    display data into FilmsList component

--- src/pages/films/filmsList/FilmsList.js
+++ src/pages/films/filmsList/FilmsList.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import style from './filmsList.module.css';
+import FilmListItem from '../../../../../etraveli/src/pages/films/filmsList/FilmListItem';
 
-function FilmsList() {
-  return <div>FilmsList</div>;
+function FilmsList({ films, onSelect, selected }) {
+  return (
+    <>
+      {films?.map((film) => (
+        <FilmListItem
+          key={film.episode_id}
+          film={film}
+          onSelect={onSelect}
+          selected={selected}
+        />
+      ))}
+    </>
+  );
 }
 
 export default FilmsList;
